293d67e8cb3ba2ae6e647a3e80edd2db64645a24
rrgayhart <rrgayhart@gmail.com>
2016-09-25 13:24:20 -0600

    standard passing

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,13 +1,15 @@
+/* global React ReactDOM */
+
 var div = React.DOM.div
 var h1 = React.DOM.h1
 
 var MyTitle = React.createClass({
   render () {
-  	return (
-  		div(null,
+    return (
+      div(null,
         h1({style: {color: this.props.color}}, this.props.title)
       )
-  	)
+    )
   }
 })
 
