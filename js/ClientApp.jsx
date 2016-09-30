855bc14e1100027c92ff4d262efbc04145ebd700
rrgayhart <rrgayhart@gmail.com>
2016-09-30 14:34:03 -0700

    refactor to fancy es6

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -2,13 +2,11 @@ var React = require('react')
 var ReactDOM = require('react-dom')
 var MyTitle = require('./MyTitle')
 
-var MyFirstComponent = function(){
-  return (
-    <div>
-      <MyTitle title='whatevs' color='rebeccapurple' />
-      <MyTitle title='ahoy' color='papayawhip' />
-    </div>
-  )
-}
+var MyFirstComponent = () => (
+  <div>
+    <MyTitle title='whatevs' color='rebeccapurple' />
+    <MyTitle title='ahoy' color='papayawhip' />
+  </div>
+)
 
 ReactDOM.render(<MyFirstComponent />, document.getElementById('app'))
